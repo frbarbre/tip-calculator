cd1e053c5faa955578e98865e68f718f479c4f28
frbarbre <fr.barbre@gmail.com>
2023-06-30 09:20:58 +0200

    overflow fix on mobile

--- app/components/Total.jsx
+++ app/components/Total.jsx
@@ -2,7 +2,7 @@ import { TipTotal, BillTotal, Reset } from ".";
 
 export default function Total(props) {
   return (
-    <div className="bg-tip-dark-cyan p-8 rounded-2xl w-full md:min-w-[360px] flex flex-col justify-between">
+    <div className="bg-tip-dark-cyan p-8 rounded-2xl w-screenCalc overflow-auto mx-auto md:w-full md:min-w-[360px] flex flex-col justify-between">
       <div>
         <TipTotal tipTotal={props.tipTotal} />
         <BillTotal billTotal={props.billTotal} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -12,15 +12,16 @@ module.exports = {
         "tip-dark-cyan": "hsl(183, 100%, 15%)",
         "tip-dark-gray": "hsl(184, 14%, 56%)",
         "tip-medium-gray": "hsl(185, 41%, 84%)",
-        "tip-light-gray": "hsl(189, 41%, 97%)"
+        "tip-light-gray": "hsl(189, 41%, 97%)",
       },
       width: {
-        max: "max-content"
-      }
+        max: "max-content",
+        screenCalc: "calc(100vw - 64px)",
+      },
     },
     screens: {
-      md: "800px"
-    }
+      md: "800px",
+    },
   },
   plugins: [],
-}
+};
